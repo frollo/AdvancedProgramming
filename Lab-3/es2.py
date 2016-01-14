26b001036e023400874e78031cb1e42cbac0d109
Lorenzo Rossi <rossi.lorenzo@yandex.com>
2016-01-14 16:27:12 +0100

    Added identity test

--- Lab-3/es2.py
+++ Lab-3/es2.py
@@ -8,3 +8,8 @@ class Monoid(object):
             testset = testset[0:threshold]
         return reduce(lambda x,y: x and y, [True if operation(operation(x,y),z) == operation(operation(x,z),y) else False\
         for x in testset for y in testset for z in testset])
+
+    def testIdentity(testset, operation, identity):
+        if len(testset) > threshold:
+            testset = testset[0:threshold]
+        return reduce(lambda x,y: x and y, [True if operation(x,identity) == x else False for x in testset])
