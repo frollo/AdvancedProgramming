eae80fe9be05ac716f5fbffad8233065733218f2
Lorenzo Rossi <rossi.lorenzo@yandex.com>
2016-01-14 16:21:03 +0100

    Started Lab 3 exercise2

---
+++ Lab-3/es2.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+class Monoid(object):
+    threshold = 100
+
+    def testAssociativity(testset, operation):
+        if len(testset) > threshold:
+            testset = testset[0:threshold]
+        return reduce(lambda x,y: x and y, [True if operation(operation(x,y),z) == operation(operation(x,z),y) else False\
+        for x in testset for y in testset for z in testset])
