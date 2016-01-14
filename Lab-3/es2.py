c09c43ef25aa95619e4777e9032046580bc3d721
Lorenzo Rossi <rossi.lorenzo@yandex.com>
2016-01-14 16:31:55 +0100

    Added the first test case and fixed class variable definition

--- Lab-3/es2.py
+++ Lab-3/es2.py
@@ -4,12 +4,28 @@ class Monoid(object):
     threshold = 100
 
     def testAssociativity(testset, operation):
-        if len(testset) > threshold:
-            testset = testset[0:threshold]
+        if len(testset) > Monoid.threshold:
+            testset = testset[0:Monoid.threshold]
         return reduce(lambda x,y: x and y, [True if operation(operation(x,y),z) == operation(operation(x,z),y) else False\
         for x in testset for y in testset for z in testset])
 
     def testIdentity(testset, operation, identity):
-        if len(testset) > threshold:
-            testset = testset[0:threshold]
+        if len(testset) > Monoid.threshold:
+            testset = testset[0:Monoid.threshold]
         return reduce(lambda x,y: x and y, [True if operation(x,identity) == x else False for x in testset])
+
+
+if __name__ == '__main__':
+    #Test set 1: {True,False}, OR, False
+    booleans = [True, False]
+    operation = lambda x,y : x or y
+    identity = False
+    if Monoid.testAssociativity(booleans, operation):
+        print ("OR is associative on boolean values!")
+    else:
+        print ("Something went wrong")
+
+    if Monoid.testIdentity(booleans, operation, identity):
+        print ("<{True,False}, OR, False> is a monoid!")
+    else:
+        print("Something went wrong")
