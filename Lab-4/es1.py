6f6824011fe5345c50e1fa5266dbe901cee69f69
Lorenzo Rossi <rossi.lorenzo@yandex.com>
2016-01-16 17:53:27 +0100

    Lab 4, first exercise, third step - first implementation

--- Lab-4/es1.py
+++ Lab-4/es1.py
@@ -6,13 +6,23 @@ class MyString(str):
         super(MyString, self).__init__()
         self.value = value
 
+    def __cleanUp__(value):
+        return re.sub("[\W]", "", value).lower()
+
     def isPalindrome(self):
-        clean = re.sub("[\W]", "", self.value)
-        clean = clean.lower()
+        clean = MyString.__cleanUp__(self.value)
         return reduce(lambda x,y: x and y, map(lambda x: x[0]==x[1], zip(clean, clean[::-1])))
 
     def subtract(self, string):
         return MyString(re.sub("[" + string + "]", "", self.value))
+
+    def __isAnagram__(self, string):
+        cleanValue = sorted(list(MyString.__cleanUp__(self.value)))
+        cleanString = sorted(list(MyString.__cleanUp__(self.value)))
+        return reduce(lambda x,y: x and y, map(lambda x: x[0]==x[1], zip(cleanValue, cleanString)))
+
+    def dictAnagram(self, dictionary):
+        return len(list(filter(lambda x: self.__isAnagram__(x), dictionary.keys()))) > 0
 
 if __name__ == '__main__':
     pals = ["Rise to vote, sir", "Do geese see God?", "detartrated"]
@@ -22,3 +32,9 @@ if __name__ == '__main__':
 
     sub = MyString("Walter Cazzola")
     print(sub.subtract(MyString("abcwxyz")))
+
+    diString = MyString("anagramma")
+    diDict1 = {"grammana" : 1, "grandma": 3}
+    diDict2 = {"chiesa": 0, "casa" : 1, "coca-cola": 2}
+    print (diString.dictAnagram(diDict1))
+    print (diString.dictAnagram(diDict2))
